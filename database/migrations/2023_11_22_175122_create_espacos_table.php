f385d68c6cec3b598d9aeae00f4829a07865bffa
Hiury <hiurygabrieltressoldi@gmail.com>
2023-12-05 00:46:41 -0300

    espacos

--- database/migrations/2023_11_22_175122_create_espacos_table.php
+++ database/migrations/2023_11_22_175122_create_espacos_table.php
@@ -15,16 +15,10 @@ return new class extends Migration
 
         Schema::create('espacos', function (Blueprint $table) {
             $table->id();
-<<<<<<< HEAD
             $table->char('nome', 50);
             $table->char('esporte', 50);
             $table->char('endereco', 255);
             $table->char('descricao', 255);
-=======
-            $table->char('nome', 255);
-            $table->char('endereco', 100);
-            $table->char('descricao', 100);
->>>>>>> 173d9a457c8d8efb67b5573996a4a010fdfd87d9
             $table->char('foto');
             $table->float('valorHora');
             $table->timestamps();
